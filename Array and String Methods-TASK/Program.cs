dbbcf470755b60654d31601847dfcd6a73f93ac5
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-15 12:34:09 +0400

    Methods Task ADDED

---
+++ Array and String Methods-TASK/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Array_and_String_Methods_TASK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "bu taski methodla yazmalisiz";
+            string newSentence = sentence;
+
+            string sen = sentence.Replace("bu taski methodla yazmalisiz", "Bu Taski Methodla Yazmalisiz");
+            
+            Console.WriteLine(sen);
+        }
+    }
+}
